e3ca84daa33588af9a6e77b6381f9d772d30279c
atom32 <lordvv2010@gmail.com>
2026-03-12 12:31:23 +0800

    cleanup: Remove debug logs from main_menu.gd

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -7,29 +7,15 @@ extends Control
 @onready var quit_button = $CanvasLayer/CenterContainer/VBoxContainer/QuitButton
 
 func _ready():
-	print("MainMenu: _ready() called")
-	print("Start button node: ", start_button)
-	print("Quit button node: ", quit_button)
-
 	# Reset all game systems when returning to main menu
 	_reset_game_state()
 
 	# Connect button signals
 	if start_button:
-		print("Connecting start button signal...")
 		start_button.pressed.connect(_on_start_button_pressed)
-		print("Start button signal connected")
-	else:
-		print("ERROR: Start button is null!")
 
 	if quit_button:
-		print("Connecting quit button signal...")
 		quit_button.pressed.connect(_on_quit_button_pressed)
-		print("Quit button signal connected")
-	else:
-		print("ERROR: Quit button is null!")
-
-	print("Main Menu loaded")
 
 ## Reset all game state when returning to main menu
 func _reset_game_state():
@@ -49,5 +35,4 @@ func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
 
 func _on_quit_button_pressed():
-	print("Quitting game...")
 	get_tree().quit()
